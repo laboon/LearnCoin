30fa77e5bd1012305b66b6dccd85a36d3a43904a
laboon <laboon@gmail.com>
2017-06-15 16:00:49 -0400

    add some framework classes

---
+++ src/main/java/laboon/Block.java
@@ -0,0 +1,18 @@
+public class Block {
+
+    public long index;
+
+    public long timestamp;
+
+    public Transaction[] data;
+
+    public String prevHash;
+
+    public String hash;
+    
+
+    public boolean verify() {
+	// TODO - add
+	return true;
+    }
+}
